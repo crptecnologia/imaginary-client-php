dafadfe7cc9a41909c8d9690fa936ba9083a9841
Dejair Sisconeto <dejair.sisconeto@crptecnologia.com.br>
2019-04-16 15:52:25 -0300

    criando exception personalizada

--- src/Pipeline/PipelineFactoryInterface.php
+++ src/Pipeline/PipelineFactoryInterface.php
@@ -2,7 +2,11 @@
 
 namespace CrpTecnologia\ImaginaryClient\Pipeline;
 
+use CrpTecnologia\ImaginaryClient\DefaultConfiguration;
+
 interface PipelineFactoryInterface
 {
     public function make(string $source): Pipeline;
+
+    public function setDefaultConfiguration(DefaultConfiguration $defaultConfiguration): void;
 }
